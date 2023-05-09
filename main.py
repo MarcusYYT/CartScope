95bef967f5dcecb0fcd291a6f5b0c9f1f138e27b
Mars <marssong66@gmail.com>
2023-05-09 16:46:45 -0700

    FEAT: Delete comments

--- main.py
+++ main.py
@@ -9,34 +9,6 @@ columns = 21
 cart_num = 5
 items = {}
 
-# # Brute force implementation of tsp
-# def tsp(cities):
-#     # Generate all possible permutations of the cities
-#     permutations = itertools.permutations(cities)
-#
-#     # Initialize variables for the shortest distance and route
-#     shortest_distance = float('inf')
-#     shortest_route = None
-#
-#     # Iterate over each permutation and calculate its distance
-#     for route in permutations:
-#         route_distance = 0
-#         for i in range(len(route) - 1):
-#             route_distance += distance(route[i], route[i + 1])
-#         # Add the distance from the last city back to the first city
-#         route_distance += distance(worker, route[0])
-#         route_distance += distance(worker, route[len(route)-1])
-#
-#         # Update the shortest distance and route if necessary
-#         if route_distance < shortest_distance:
-#             shortest_distance = route_distance
-#             shortest_route = route
-#
-#     return shortest_route, shortest_distance
-
-
-
-
 worker = ()
 carts = []
 nodes = [[0 for i in range(columns)] for i in range(rows)]
