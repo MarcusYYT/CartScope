54b254188557f03e3a7da3d18aa6ed8b13d1a3cd
Mars <marssong66@gmail.com>
2023-05-23 16:27:18 -0700

    FEAT: First version of implementing the greedy algorithm

--- tsp.py
+++ tsp.py
@@ -1,5 +1,7 @@
 
 import itertools
+import sys
+
 import route_generator
 
 # Calculate the distance between two nodes
@@ -47,4 +49,26 @@ def branch_tsp(worker, nodes, items):
     return None, 0
 
 def greedy_tsp(worker, nodes, items):
-    return None, 0
+    route = []
+    dis = 0
+    cur_node  = worker
+    item_set = {}
+    # Add all items into a hashmap
+    for i in range(len(items)):
+        item_set[items[i]] = True
+    # Traverse all items
+    for i in range(len(items)):
+        cur_dis = sys.maxsize
+        cur_item = -1
+        # Go through items to find the nearest neighbor
+        for j in range(len(items)):
+            # Only traverse the items that has not been picked up
+            if(item_set[items[j]]):
+                new_dis = distance(nodes, cur_node, items[j])
+                if(new_dis<cur_dis):
+                    cur_item = items[j]
+                    cur_dis = new_dis
+        route.append(cur_item)
+        dis += cur_dis
+        del item_set[cur_item]
+    return route, dis
