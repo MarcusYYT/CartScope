b4d0853c11b198a0ebf986ab666828383c689599
Mars <marssong66@gmail.com>
2023-06-01 09:32:19 -0700

    DEBUG: Fix Move bugs

--- tsp.py
+++ tsp.py
@@ -1,7 +1,7 @@
 import math
 import sys
 import copy
-from queue import PriorityQueue
+from queue import PriorityQueue, Queue
 import route_generator
 INF = sys.maxsize
 rows = 40
@@ -124,6 +124,8 @@ def reduce_matrix(matrix):
             continue
         res += col_mins[i]
         for j in range(len(col_mins)):
+            if reduced_matrix[j][i] == INF:
+                continue
             reduced_matrix[j][i] -= col_mins[i]
     return res, reduced_matrix
 
@@ -179,7 +181,7 @@ def tsp_branch_bound(dist_matrix):
 
 # This function is to move from one point to another and do some pre settings
 def move(matrix, x, y):
-    moveMatrix = list(matrix)
+    moveMatrix = copy.deepcopy(matrix)
     for i in range(len(matrix)):
         moveMatrix[x][i] = INF
         moveMatrix[i][y] = INF
@@ -193,6 +195,8 @@ access_shelf = {}
 # list of access points
 access_points = []
 
+start = ()
+
 
 def get_shelves(items, nodes, shelves):
     for item in items:
@@ -204,7 +208,9 @@ def get_shelves(items, nodes, shelves):
 
 
 def multi_branch_tsp(worker, nodes, items):
+    global start
     id = 1
+    start = worker
     shelves = []
     get_shelves(items, nodes, shelves)
     for shelf in shelves:
@@ -214,6 +220,8 @@ def multi_branch_tsp(worker, nodes, items):
             access_shelf[loc] = {"shelf": shelf, "id": id}
             access_points.append(loc)
             id += 1
+    access_shelf[worker] = {'shelf': worker, "id": 0}
+    shelf_access[worker] = [worker]
     n = len(access_points)
     dist_matrix = [[INF] * (n + 1) for _ in range(n + 1)]
     # Construct the distance matrix
@@ -229,17 +237,31 @@ def multi_branch_tsp(worker, nodes, items):
             dist = distance(nodes, p1, p2)
             dist_matrix[i][j] = dist
             dist_matrix[j][i] = dist
-    route, dis = branch_and_bound_multi(dist_matrix, worker, len(shelf)+1)
+    # print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in dist_matrix]))
+    route, dis = branch_and_bound_multi(dist_matrix, worker, len(shelves)+1)
     # Remove worker location for the generality of the API
-    print(route, dis)
     route.remove(worker)
     return route, dis
+
+
+def get_dis(dist_matrix, path):
+    dis = 0
+    for i in range(1, len(path)):
+        pre = access_shelf[path[i-1]]['id']
+        cur = access_shelf[path[i]]['id']
+        dis += dist_matrix[pre][cur]
+    start = access_shelf[path[0]]['id']
+    end = access_shelf[path[len(path)-1]]['id']
+    dis += dist_matrix[start][end]
+    return dis
+
 
 def branch_and_bound_multi(dist_matrix, worker, n):
     matrix = []
     start_size_matrix = sys.getsizeof(matrix)
     bound, matrix = reduce_matrix(dist_matrix)
-    pq = PriorityQueue()
+    # print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
+    pq = Queue()
     start_size_queue = sys.getsizeof(pq)
     root = {
         'bound': bound,
@@ -257,13 +279,25 @@ def branch_and_bound_multi(dist_matrix, worker, n):
         for i in range(size):
             node = pq.get()
             # End condition
-            print(len(node['path']))
             if len(node['path']) == n:
                 # end_size_matrix = sys.getsizeof(matrix)
                 # end_size_queue = sys.getsizeof(pq)
                 # print(
                 #     f"Memory usage of current run using Batch&Bound Algorithm: {end_size_queue + end_size_matrix - start_size_matrix - start_size_queue}")
-                return node['path'], node['bound']
+                final_distance = get_dis(dist_matrix, node['path'])
+                while not pq.empty():
+                    tmp_node = pq.get()
+                    tmp_dis = get_dis(dist_matrix, tmp_node['path'])
+                    if tmp_dis < final_distance:
+                        node = tmp_node
+                        final_distance = tmp_dis
+                # print(size)
+                return node['path'], final_distance
+            tmp_bound = INF
+            tmp_offset = 0
+            tmp_list = []
+            pre_shelf = None
+            pre_base = 0
             for j in range(1, len(dist_matrix)):
                 if j == node['num']:
                     continue
@@ -271,25 +305,58 @@ def branch_and_bound_multi(dist_matrix, worker, n):
                 # print(shelf)
                 if shelf in node['visited']:
                     continue
-                newBound, newMatrix = move(node['matrix'], node['num'], j)
+                newBound, newMatrix = move_multi(node['matrix'], node['num'], j)
                 # Compute new bounds
                 newVal = node['bound'] + dist_matrix[node['num']][j] + newBound
-                # print(newVal, access_points[j-1])
-                if newVal < curBound:
-                    curBound = newVal
-                    arr.clear()
-                    newPath = copy.deepcopy(node['path'])
-                    newPath.append(access_points[j-1])
-                    newVisit = copy.deepcopy(node['visited'])
-                    newVisit.add(shelf)
-                    arr.append([j, newMatrix, newPath, newVisit])
-                elif newBound == curBound:
-                    newPath = copy.deepcopy(node['path'])
-                    newPath.append(access_points[j])
-                    newVisit = copy.deepcopy(node['visited'])
-                    newVisit.add(shelf)
-                    arr.append([j, newMatrix, newPath, newVisit])
+                print(newBound, access_points[j-1], dist_matrix[node['num']][j])
+                newPath = copy.deepcopy(node['path'])
+                newPath.append(access_points[j - 1])
+                newVisit = copy.deepcopy(node['visited'])
+                newVisit.add(shelf)
+                if shelf == pre_shelf:
+                    dis = calculate_dis(dist_matrix, j, node['visited'])
+                    # print(dis, pre_base)
+                    offset = dis - pre_base
+                    if newVal+offset < tmp_bound:
+                        tmp_bound = newVal+offset
+                        tmp_offset = offset
+                        tmp_list.clear()
+                        tmp_list.append([j, newMatrix, newPath, newVisit])
+                    elif newVal+offset == tmp_bound:
+                        if offset >= tmp_offset:
+                            if offset > tmp_offset:
+                                tmp_list.clear()
+                                tmp_offset = offset
+                            tmp_list.append([j, newMatrix, newPath, newVisit])
+                else:
+                    # print(shelf)
+                    pre_shelf = shelf
+                    pre_base = calculate_dis(dist_matrix, j, node['visited'])
+                    if tmp_bound != INF:
+                        pre_bound = tmp_bound-tmp_offset
+                        if pre_bound < curBound:
+                            curBound = pre_bound
+                            arr.clear()
+                            for item in tmp_list:
+                                arr.append(item)
+                        elif newBound == curBound:
+                            for item in tmp_list:
+                                arr.append(item)
+                    tmp_bound = newVal
+                    tmp_offset = 0
+                    tmp_list.clear()
+                    tmp_list.append([j, newMatrix, newPath, newVisit])
+            pre_bound = tmp_bound - tmp_offset
+            if pre_bound < curBound:
+                curBound = pre_bound
+                arr.clear()
+                for item in tmp_list:
+                    arr.append(item)
+            elif newBound == curBound:
+                for item in tmp_list:
+                    arr.append(item)
         # Add potential nodes into the queue for future traverse
+        # print(len(arr))
         for i in range(len(arr)):
             pq.put({
                 'bound': curBound,
@@ -304,26 +371,35 @@ def branch_and_bound_multi(dist_matrix, worker, n):
     return None, -1
 
 def get_shelf(num):
+    if num == 0:
+        return start
     access_point = access_points[num-1]
     return access_shelf[access_point]['shelf']
 
-# TODO: Set all access points for one item
 def move_multi(matrix, x, y):
     shelf_x = get_shelf(x)
     shelf_y = get_shelf(y)
+
     points_x = shelf_access[shelf_x]
     points_y = shelf_access[shelf_y]
-    moveMatrix = list(matrix)
-    for i in range(len(matrix)):
-        for j in range(len(points_x)):
-            point = points_x[j]
-            x_id = access_shelf[point]['id']
-            moveMatrix[x_id][i] = INF
-        for j in range(len(points_y)):
-            point = points_y[j]
-            y_id = access_shelf[point]['id']
-            moveMatrix[i][y_id] = INF
+    moveMatrix = copy.deepcopy(matrix)
+    for i in range(len(points_x)):
+        point = points_x[i]
+        x_id = access_shelf[point]['id']
+        # print(x_id)
+        for j in range(len(matrix)):
+            moveMatrix[x_id][j] = INF
+    for i in range(len(points_y)):
+        point = points_y[i]
+        y_id = access_shelf[point]['id']
+        for j in range(len(matrix)):
+            moveMatrix[j][y_id] = INF
     # print(moveMatrix)
+    if shelf_y == (20, 10):
+        print(x, y)
+        print(points_x)
+        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
+        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in moveMatrix]))
     return reduce_matrix(moveMatrix)
 
 dir_matrix = [1, 0, -1, 0, 1]
@@ -342,3 +418,26 @@ def getLoc(location, nodes):
 def isValid(point, nodes):
     x, y = point
     return 0 <= x < len(nodes)  and 0 <= y < len(nodes[0])
+
+def calculate_dis(dist_matrix, num, visited):
+    shelf = get_shelf(num)
+    pre = None
+    res = 0
+    cnt = 0
+    sum = 0
+    for i in range(1, len(dist_matrix)):
+        tmp = get_shelf(i)
+        if tmp == shelf or tmp in visited:
+            continue
+        if tmp == pre:
+           sum += dist_matrix[num][i]
+           cnt += 1
+        else:
+            if cnt != 0:
+                res += sum/cnt
+            sum = dist_matrix[num][i]
+            cnt = 1
+    if cnt!= 0:
+        res += sum/cnt
+    # print(res, num)
+    return res
